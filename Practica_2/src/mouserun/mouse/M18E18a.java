ef6079f20787fab8ae7505f761461012b04b8222
Abdallah Abedraba <aabedraba@gmail.com>
2018-02-13 20:04:20 +0100

    Nuevo ratón explorador

---
+++ Practica_2/src/mouserun/mouse/M18E18a.java
@@ -0,0 +1,31 @@
+package mouserun.mouse;
+
+import mouserun.game.*;
+import java.util.*;
+
+//TODO eliminar mensajes depuración
+
+public class M18E18a extends Mouse {
+
+    /**
+     * Constructor
+     */
+    public M18E18a(){
+        super("Ratotuille");
+    }
+
+    @Override
+    public int move(Grid currentGrid, Cheese cheese) {
+        return 0;
+    }
+
+    @Override
+    public void newCheese() {
+
+    }
+
+    @Override
+    public void respawned() {
+
+    }
+}
