74d669e9c82d877bde9cfb2f2fe5bfc0fe1dce1a
Sergio <red17.sj@gmail.com>
2018-02-17 13:09:39 +0100

    ESTA VIVO!!!!!!!!!!! E cambiado las funciones para moverse, pero es un poco lento, aun hay situaciones que lo bloquean. Un pequeño comienzo.

--- Practica_2/src/mouserun/mouse/M18E18a.java
+++ Practica_2/src/mouserun/mouse/M18E18a.java
@@ -8,6 +8,8 @@ import java.util.*;
 public class M18E18a extends Mouse {
 
     private ArrayList<Grid> celdasVisitadas;
+    private Grid nextGrid;
+    private Grid lastGrid;
     /**
      * Constructor
      */
@@ -20,32 +22,100 @@ public class M18E18a extends Mouse {
     @Override
     public int move(Grid currentGrid, Cheese cheese) {
         celdasVisitadas.add( currentGrid );
-
         return busqueda(currentGrid);
     }
 
     public int busqueda( Grid currentGrid ) {
-        boolean control=true;
-        if (currentGrid.canGoUp()) {
-            for (int i = 0; i < celdasVisitadas.size(); i++) {
-                if (celdasVisitadas.get(i).getY() == currentGrid.getY() + 1) control=false;
+        ArrayList<Integer> posiblesMov = new ArrayList<Integer>();
+		if (currentGrid.canGoUp()) posiblesMov.add(Mouse.UP);
+		if (currentGrid.canGoDown()) posiblesMov.add(Mouse.DOWN);
+		if (currentGrid.canGoLeft()) posiblesMov.add(Mouse.LEFT);
+		if (currentGrid.canGoRight()) posiblesMov.add(Mouse.RIGHT);
+        if (posiblesMov.size() == 1){
+            lastGrid = currentGrid;
+            return posiblesMov.get(0);
+        }else{
+            for(int i=0;i<posiblesMov.size();i++){
+                if(Prueba(posiblesMov.get(i),currentGrid)){
+                    lastGrid = currentGrid;
+                    return posiblesMov.get(i);
+                }      
             }
-            if(control) return Mouse.UP;
         }
-        if (currentGrid.canGoRight()) {
-            for (int i = 0; i < celdasVisitadas.size(); i++) {
-                if (celdasVisitadas.get(i).getX() == currentGrid.getX() + 1) control=false;
+        for(int i=0;i<posiblesMov.size();i++){
+            if(Vuelta(posiblesMov.get(i),currentGrid)){
+                lastGrid = currentGrid;
+                return posiblesMov.get(i);
             }
-            if(control) return Mouse.RIGHT;
         }
-        if (currentGrid.canGoLeft()) {
-            for (int i = 0; i < celdasVisitadas.size(); i++) {
-                if (celdasVisitadas.get(i).getX() != currentGrid.getX() - 1) control=false;
-            }
-            if(control) return Mouse.LEFT;
-        }
-        return Mouse.DOWN;
+        return Mouse.BOMB;
     }
+
+    private boolean Prueba (int direction, Grid currentGrid){
+        boolean control=true;
+        if (celdasVisitadas.size() == 1){
+            return true;
+        }else{
+            int x = currentGrid.getX();
+            int y = currentGrid.getY();
+		
+            switch (direction){
+                case Mouse.UP: 
+                    y += 1;
+                    break;
+				
+                case Mouse.DOWN:
+                    y -= 1;
+                    break;
+				
+                case Mouse.LEFT:
+                    x -= 1;
+                    break;
+				
+                case Mouse.RIGHT:
+                    x += 1;
+                    break;
+            }
+		
+            for (int i = 0; i < celdasVisitadas.size(); i++) {
+                    if (celdasVisitadas.get(i).getX() == x &&
+                        celdasVisitadas.get(i).getY() == y){
+                        control=false;
+                    }
+                }
+                
+            return control;
+        }
+    }
+    
+    private boolean Vuelta (int direction, Grid currentGrid){
+        boolean control=true;
+        int x = currentGrid.getX();
+        int y = currentGrid.getY();
+
+        switch (direction){
+            case Mouse.UP: 
+                y += 1;
+                break;
+
+            case Mouse.DOWN:
+                y -= 1;
+                break;
+
+            case Mouse.LEFT:
+                x -= 1;
+                break;
+
+            case Mouse.RIGHT:
+                x += 1;
+                break;
+        }
+            if (lastGrid.getX() == x && lastGrid.getY() == y){
+                control=false;
+            }
+        return control;
+    }
+  
 
     @Override
     public void newCheese() {
