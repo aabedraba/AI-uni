d27ccb95072e991abd4215ea75454a2743bc16e5
Abdallah Abedraba <aabedraba@gmail.com>
2018-04-03 13:43:26 +0200

    Nueco cruce

--- flappy/genetic.js
+++ flappy/genetic.js
@@ -145,17 +145,25 @@ GeneticAlgorithm.prototype = {
 	
 	// performs a single point crossover between two parents
 	crossOver : function(parentA, parentB) {
+		var division = parentA.neurons.length / 2;
+		var primerPunto = this.random(1, division-1); //punto izquierdo no es igual a 0
+		var segundoPunto = this.random(division+1, parentA.neurons.length-2); //punto derecho no es igual al tamaño
+		for (var i = primerPunto; i <= segundoPunto; i++ ) {
+			var auxiliar = parentA.neurons[i]['bias'];
+			parentA.neurons[i]['bias'] = parentB.neurons[i]['bias'];
+			parentB.neurons[i]['bias'] = auxiliar;
+		}
 		// get a cross over cutting point
-		var cutPoint = this.random(0, parentA.neurons.length-1);
+		// var cutPoint = this.random(0, parentA.neurons.length-1);
 		
 		// swap 'bias' information between both parents:
 		// 1. left side to the crossover point is copied from one parent
 		// 2. right side after the crossover point is copied from the second parent
-		for (var i = cutPoint; i < parentA.neurons.length; i++){
-			var biasFromParentA = parentA.neurons[i]['bias'];
-			parentA.neurons[i]['bias'] = parentB.neurons[i]['bias'];
-			parentB.neurons[i]['bias'] = biasFromParentA;
-		}
+		// for (var i = cutPoint; i < parentA.neurons.length; i++){
+		// 	var biasFromParentA = parentA.neurons[i]['bias'];
+		// 	parentA.neurons[i]['bias'] = parentB.neurons[i]['bias'];
+		// 	parentB.neurons[i]['bias'] = biasFromParentA;
+		// }
 
 		return this.random(0, 1) == 1 ? parentA : parentB;
 	},
